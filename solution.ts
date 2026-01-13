820d533c1728bce25d55c26601d1198564e8c445
nurnewas <nurnewasjr@gmail.com>
2026-01-14 01:46:25 +0600

    Problem solved 3

--- solution.ts
+++ solution.ts
@@ -21,15 +21,31 @@ const formatValue = (input: InputValue) => {
 // Problem 2
 type inputType = string | any[];
 
-const getLength = (input: inputType):number => {
+const getLength = (input: inputType): number => {
     if (typeof input === "string") {
         return input.length;
     }
     else if (Array.isArray(input)) {
         return input.length;
-    } 
-        return 0
+    }
+    return 0
 }
 
-console.log(getLength('typescript'));
-console.log(getLength([10, 20, 30, 40]));
+// Problem 3
+class Person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age
+    }
+    getDetails(): string {
+        return `Name: ${this.name}, Age: ${this.age}`;
+    }
+}
+
+const person1 = new Person('John Doe', 30);
+console.log(person1.getDetails());
+
+const person2 = new Person('Alice', 25);
+console.log(person2.getDetails());
