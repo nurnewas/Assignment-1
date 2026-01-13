56194df3bafb87196e03e5b6e22e4870b159e127
nurnewas <nurnewasjr@gmail.com>
2026-01-14 00:57:01 +0600

    problem 1.1

--- solution.ts
+++ solution.ts
@@ -1,1 +1,9 @@
 // Problem 1
+type InputValue = string | number | boolean
+
+const formatValue = (input: InputValue) => {
+    if (typeof input === "string") {
+        return input.toUpperCase();
+    }
+}
+console.log(formatValue('hello'));
